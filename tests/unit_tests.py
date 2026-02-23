a574faa73f7f0e299fd281adee5f2e670ee21df3
Kristoffer Andersson <kristoffer.andersson@gu.se>
2026-02-23 12:30:51 +0100

    test: fix formatting in unit_tests.test_code_blocks

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1535,8 +1535,8 @@ for name and age:</p>
 </div>'''
     testresult = extract(w3schools, config=ZERO_CONFIG, output_format='xml')
     expected = '''<code>
-  class Person:<lb/>\xa0 def __init__(self, name, age):<lb/>\xa0\xa0\xa0
-  self.name = name<lb/>\xa0\xa0\xa0 self.age = age<lb/><lb/>p1 = Person("John",
+  class Person:<lb/>\xa0 def __init__(self, name, age):<lb/>\xa0\xa0\xa0\x20
+  self.name = name<lb/>\xa0\xa0\xa0 self.age = age<lb/><lb/>p1 = Person("John",\x20
   36)<lb/>
   <lb/>print(p1.name)<lb/>print(p1.age) </code>'''
     assert expected in testresult and 'quote' not in testresult
